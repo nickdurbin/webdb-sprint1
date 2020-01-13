db58efe34e7a2983c5cde041c2fe3bef9f08774f
ndurbin <nick.durbin@gmail.com>
2020-01-13 05:15:03 -0500

    Added initial seed data for testing purposes.

--- data/seeds/01_resources.js
+++ data/seeds/01_resources.js
@@ -1,13 +1,8 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('resources')
     .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('resources').insert([
+        {id: 1, resource_name: 'Express', resource_description: "A powerful tool to help Node."},
       ]);
     });
 };
