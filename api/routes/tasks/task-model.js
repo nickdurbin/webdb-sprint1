784cd4e13831541b18b77faeab75a48a85078cd6
ndurbin <nick.durbin@gmail.com>
2020-01-13 05:14:15 -0500

    Added the extra check for true and false

--- api/routes/tasks/task-model.js
+++ api/routes/tasks/task-model.js
@@ -1,19 +1,24 @@
 const db = require('../../../data/db-config')
 
 async function findTasks() {
-  return await db("tasks")
+  const tasks = await db("tasks").select()
+  return tasks.map(task => {
+    return { ...task, task_complete: task.task_complete === 1 ? true : false }
+  })
 }
 
 async function findTaskById(id) {
+  const task = await db("tasks").where({ id }).first()
+  return { ...task, task_complete: task.task_complete === 1 ? true : false }
+}
+
+async function addTask(newTask) {
+  const [id] = await db("tasks").insert(newTask)
   return await db("tasks").where({ id }).first()
 }
 
-async function addTask() {
-  return await db("tasks")
-}
-
-async function updateTask() {
-  return await db("tasks")
+async function updateTask(changes, id) {
+  return await db("tasks").where({ id }).update(changes)
 }
 
 async function deleteTask(id) {
