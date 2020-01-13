5386cba580feeec28ac8d2a3bd7f15734637c3ad
ndurbin <nick.durbin@gmail.com>
2020-01-13 02:54:54 -0500

    Added the truncating of the seed data and tables.

--- data/seeds/00_cleanup.js
+++ data/seeds/00_cleanup.js
@@ -1,13 +1,8 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+exports.seed = async (knex) => {
+  await knex("tasks_contexts").truncate()
+  await knex("tasks").truncate()
+  await knex("contexts").truncate()
+  await knex("projects_resources").truncate()
+  await knex("projects").truncate()
+  await knex("resources").truncate()
+}
